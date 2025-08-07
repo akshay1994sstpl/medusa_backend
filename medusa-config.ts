175a8b885d1f48dce06bd8ffc214c7ee6a3580f3
Akshay_Sensixtech <sensixtech_m4max001@Akshay---Sensixtech.local>
2025-08-07 21:49:10 +0530

    deployment changes

--- medusa-config.ts
+++ medusa-config.ts
@@ -2,23 +2,18 @@ import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
+
+
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-      // The port for the Medusa server
-      workerMode: process.env.MEDUSA_WORKER_MODE as
-        | "shared"
-        | "worker"
-        | "server",
-      admin: {
-        disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
-      },
     },
   },
 });
